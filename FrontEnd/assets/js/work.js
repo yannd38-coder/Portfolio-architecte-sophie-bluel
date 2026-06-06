f60f7e36c11ed56a7dfd122a0f2f0025ff3453b0
yannd38-coder <yann.dicko@gmail.com>
2026-06-06 10:19:57 +0200

    add gallery inner

---
+++ FrontEnd/assets/js/work.js
@@ -0,0 +1,20 @@
+async function getWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    const works = await response.json();//je viens interroger la base donnée pour une reponse en tableau d'objet
+    console.log(works);
+    displayWorks(works);
+}
+
+function displayWorks(works) {
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+
+    //mettre place boucle foreach sur works
+    //créer une figure(element html) avec legende dedans pour chaque boucle
+    //utiliser inner html sur gallery pour pouvoir rajouter dedans. donc mettre += a la place d'egal = seul pour eviter que les figures soient supprimé les un a la suite
+
+    works.forEach(work => {
+        gallery.innerHTML += `<figure><img src= "${work.imageUrl}" alt="${work.title}"><figcaption>${work.title}</figcaption></figure>`
+    })
+}
+getWorks()
--- FrontEnd/work.js
+++
@@ -1,7 +0,0 @@
-async function getWorks() {
-    const response = await fetch("http://localhost:5678/api/works");
-    const works = await response.json();
-    console.log(works);
-}
-
-
